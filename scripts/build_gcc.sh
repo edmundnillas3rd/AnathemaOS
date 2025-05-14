a178b6a02edc6ef7ae3417c0863f7f7d962d0b58
Edmund Nillas III <edmundnillasiii@gmail.com>
2025-05-14 13:46:09 +0800

    Refactor build script for gcc

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/sh
 
-BINUTILS_MAJOR_VERSION=2 # Ubuntu only has this particular version, and I don't want to work with snapd
+BINUTILS_MAJOR_VERSION=2 
 BINUTILS_MINOR_VERSION=38
 BINUTILS_PACKAGE_NAME="binutils-$BINUTILS_MAJOR_VERSION.$BINUTILS_MINOR_VERSION"
 BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/$BINUTILS_PACKAGE_NAME.tar.gz"
 
-if [ ! -d "$BINUTILS_PACKAGE_NAME" ]; then
-    mkdir -v "$BINUTILS_PACKAGE_NAME-build"
-    curl -LO "$BINUTILS_URL"
-fi
+curl "$BINUTILS_URL" | tar -xvf 
 
 GCC_MAJOR_VERSION=11
 GCC_MINOR_VERSION=4.0
 GCC_PACKAGE_NAME="gcc-$GCC_MAJOR_VERSION.$GCC_MINOR_VERSION"
 GCC_URL="https://ftp.gnu.org/gnu/gcc/$GCC_PACKAGE_NAME/$GCC_PACKAGE_NAME.tar.gz"
 
-if [ ! -d "$GCC_PACKAGE_NAME" ]; then
-    mkdir -v "$GCC_PAGKAGE_NAME-build"
-    curl -LO "$GCC_URL"
-fi
-
-tar -xvf "$BINUTILS_PACKAGE_NAME.tar.gz"
-tar -xvf "$GCC_PACKAGE_NAME.tar.gz"
-
-rm -r "bintuls-*"
-rm -r "gcc-*" 
+curl "$GCC_URL" | tar -xvf 
