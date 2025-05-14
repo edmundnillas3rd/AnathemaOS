5b79afe730f01b8bb33a54853875ef0a126a7355
Edmund Nillas III <edmundnillasiii@gmail.com>
2025-05-14 16:43:04 +0800

    Remove include_directories from configuration and refactor build script for gcc

--- scripts/build_gcc.sh
+++ scripts/build_gcc.sh
@@ -5,9 +5,10 @@ GCC_MINOR_VERSION=4.0
 GCC_PACKAGE_NAME="gcc-$GCC_MAJOR_VERSION.$GCC_MINOR_VERSION"
 GCC_URL="https://ftp.gnu.org/gnu/gcc/$GCC_PACKAGE_NAME/$GCC_PACKAGE_NAME.tar.gz"
 
-
 push "toolchain/tarballs"
     curl -LO "$GCC_URL" 
 
-    tar -xvz $GCC_PACKAGE_NAME 
+    pushd ".."
+	tar -xvzf "tarballs/$GCC_PACKAGE_NAME.tar.gz" -C "build"
+    popd
 popd
