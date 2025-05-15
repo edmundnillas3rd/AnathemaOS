b9f4478d0dd21007ec97c77b5211c38652a17e54
Edmund Nillas III <edmundnillasiii@gmail.com>
2025-05-15 17:10:37 +0800

    Add build script for qemu

---
+++ scripts/build_qemu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+QEMU_MAJOR_VERSION=1
+QEMU_MINOR_VERSION=6.2
+QEMU_PACKAGE_NAME="qemu-$QEMU_MAJOR_VERSION.$QEMU_MINOR_VERSION"
+QEMU_URL="https://download.qemu.org/$QEMU_PACKAGE_NAME.tar.xz"
+
+pushd "toolchain/tarballs"
+    if [ ! -e "$QEMU_PACKAGE_NAME.tar.xz" ]; then
+	curl -LO "$QEMU_URL" 
+    fi
+popd
+
+pushd "toolchain"
+    if [ ! -d "$QEMU_PACKAGE_NAME" ]; then
+	echo "Extracting file..."
+	tar -xvJf "tarballs/$QEMU_PACKAGE_NAME.tar.xz"
+    fi
+
+    mkdir -p "$QEMU_PACKAGE_NAME/build_$QEMU_PACKAGE_NAME"
+    pushd "$QEMU_PACKAGE_NAME/build_$QEMU_PACKAGE_NAME"
+        /bin/bash ../configure --target-list="i386-softmmu x86_64-softmmu" --prefix="$PREFIX" --python="/usr/bin/python2" --disable-werror --disable-xen --disable-kvm
+        make -j2
+	make -j2 install
+    popd
+popd
