1d07dd5691425dff915dfc2e952d6a5c1b300d8c
Edmund Nillas III <edmundnillasiii@gmail.com>
2025-05-24 21:44:19 +0800

    Add target platform (default: i386) for cmake builds

--- scripts/run_image.sh
+++ scripts/run_image.sh
@@ -3,4 +3,4 @@
 cp "bin/$OS_BINARY_NAME.bin" "isodir/boot/$OS_BINARY_NAME.bin"
 
 $TOOLCHAIN_PATH/grub-mkrescue -o "bin/$OS_PROJECT_NAME.iso" isodir
-$TOOLCHAIN_PATH/qemu-system-i386 -cdrom "bin/$OS_PROJECT_NAME.iso"
+$TOOLCHAIN_PATH/qemu-system-$TARGET -cdrom "bin/$OS_PROJECT_NAME.iso"
